786201b6e8f1e231b6fa701a3df5e3f8270ca12d
SergeyRomanov9896 <automag2016@mail.ru>
2026-03-12 18:51:00 +0300

    Initial

---
+++ src/masks.py
@@ -0,0 +1,29 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Маскирует номер кредитной карты, заменяет 6 средних цифр на звездочки.
+    Преобразует 16-значный номер карты в форматированную строку с замаскированными цифрами.
+
+    Args:
+        card_number (str): 16-значный номер кредитной карты.
+
+    Returns:
+        str: Замаскированный номер карты, отформатированный с пробелами в группах по 4 цифры.
+    """
+    number_disguise = card_number[6:12]
+    disguise = card_number.replace(number_disguise, "******")
+    separation = disguise[0:4] + " " + disguise[4:8] + " " + disguise[8:12] + " " + disguise[-4:]
+    return separation
+
+
+def get_mask_account(account: str) -> str:
+    """
+    Маскирует номер банковского счета, заменяя все символы, кроме последних четырех, на звездочки.
+
+    Args:
+        account (str): Номер банковского счета в виде строки.
+    Returns:
+        str: Замаскированный номер счета, где все символы, кроме последних четырех заменены на звездочки.
+    """
+    number_disguise = account[14:16]
+    disguise = account.replace(number_disguise, "**")
+    return disguise[-6:]
