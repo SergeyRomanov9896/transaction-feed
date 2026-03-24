09da2069f74d3d7bd7408d170e3f551bf2ce2844
SergeyRomanov9896 <automag2016@mail.ru>
2026-03-24 20:04:14 +0300

    add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -13,3 +13,15 @@ def filter_by_state(words: list[dict], state: str = "EXECUTED") -> list[dict]:
     return words_state
 
 
+def sort_by_date(words: list[dict], descending: bool = True) -> list[dict]:
+    """
+    Сортирует список словарей по ключу «date».
+
+    Args:
+        words (list[dict]): Список словарей для сортировки.
+        descending (bool): По умолчанию сортирует в порядке убывания (от новых к старым).
+
+    Returns:
+        list[dict]: Отсортированный список словарей по ключу «date».
+    """
+    return sorted(words, key=lambda x: x["date"], reverse=descending)
