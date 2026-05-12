51a04e0aac1419a6c8123a570dbe5c5413d50b27
SergeyRomanov9896 <automag2016@mail.ru>
2026-05-12 22:24:34 +0300

    Add logging for card and account masking functions

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,14 @@
+import logging
+
+logger = logging.getLogger("get_card")
+file_h = logging.FileHandler("logs/get_card.log", mode="w", encoding="utf-8")
+fmt = logging.Formatter("%(asctime)s | %(filename)s | %(levelname)s | %(message)s")
+
+file_h.setFormatter(fmt)
+logger.addHandler(file_h)
+logger.setLevel(logging.INFO)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """
     Маскирует номер кредитной карты, заменяет 6 средних цифр на звездочки.
@@ -13,8 +24,10 @@ def get_mask_card_number(card_number: str) -> str:
         str: Замаскированный номер карты, отформатированный с пробелами в группах по 4 цифры.
     """
     if len(card_number) != 16:
+        logger.warning("Нестандартное количество цифр в номере карты: %s", card_number)
         raise ValueError("Нестандартное количество цифр")
 
+    logger.info("Номер карты успешно замаскирован: %s...", card_number[:4])
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
@@ -32,6 +45,11 @@ def get_mask_account(account: str) -> str:
         str: Замаскированный номер счета, где все символы, кроме последних четырех заменены на звездочки.
     """
     if len(account) != 20:
+        logger.warning("Нестандартное количество цифр в номере банковского счета: %s", account)
         raise ValueError("Нестандартное количество цифр")
 
+    logger.info("Номер счета успешно замаскирован **%s", account[-4:])
     return f"**{account[-4:]}"
+
+
+print(get_mask_account("736541084301358743"))
